9a9de68c34f8beff162f74d5413dbef6bf84c124
jazzc001 <jazzcheung212@gmail.com>
2021-11-29 17:41:58 +0000

    trying to add room

--- pageloading.js
+++ pageloading.js
@@ -14,3 +14,38 @@ var timer = setInterval(function () {
   }
 }, 30);
 
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/javascript.html');
+});
+
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/swift.html');
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/css.html');
+});
+// tech namespace
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user joined ${data.room} room!`)
+    })
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
+    });
+    socket.on('discounnect', () => {
+        console.log('user disconnected');
+
+        tech.emit('message', 'user disconnected');
+
+    })
+})
