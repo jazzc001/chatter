093518cf605f94516d7f8d40b677d5c60aa928b5
jazzc001 <jazzcheung212@gmail.com>
2021-11-29 23:50:18 +0000

    chainging

--- pageloading.js
+++ pageloading.js
@@ -3,7 +3,7 @@ function eatCount() {
 }
 
 const timer = setInterval(function () {
-    let percent = 0;
+    const percent = 0;
   $(".bar").css("width", percent + "%");
   percent += 1;
   if (percent >= 100) {
