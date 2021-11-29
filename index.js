5678ad0eeebf274a7dec086cb50dbf0e206f70ba
jazzc001 <jazzcheung212@gmail.com>
2021-11-29 19:38:33 +0000

    chainging

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express()
+const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
@@ -8,59 +7,38 @@ server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-const eatCount = () => {
-  $(".monsterText").html("Click a<br>Monster to<br> Join a Room<br>to Chat");
 
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/javascript.html');
+});
+
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/swift.html');
+});
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/css.html');
+});
+// tech namespace
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user joined ${data.room} room!`)
+    })
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
     });
+    socket.on('discounnect', () => {
+        console.log('user disconnected');
 
-    app.get('/javascript', (req, res) => {
-        res.sendFile(__dirname + '/javascript.html');
-    });
+        tech.emit('message', 'user disconnected');
 
-    app.get('/swift', (req, res) => {
-        res.sendFile(__dirname + '/swift.html');
-    });
-
-    app.get('/css', (req, res) => {
-        res.sendFile(__dirname + '/css.html');
-    });
-    // tech namespace
-    const tech = io.of('/tech');
-
-    tech.on('connection', (socket) => {
-        socket.on('join', (data) => {
-            socket.join(data.room);
-            tech.in(data.room).emit('message', `New user joined ${data.room} room!`)
-        })
-        socket.on('message', (data) => {
-            console.log(`message: ${data.msg}`);
-            tech.in(data.room).emit('message', data.msg);
-        });
-        socket.on('discounnect', () => {
-            console.log('user disconnected');
-
-            tech.emit('message', 'user disconnected');
-
-        })
-    });
-};
-
-
-const timer = setInterval(() => {
-  let percent = 0;
-  $(".bar").css("width", percent + "%");
-  percent += 1;
-  if (percent >= 100) {
-    $(".pageLoading").addClass("complete");
-
-    clearInterval(timer);
-    setTimeout(eatCount, 3000);
-  }
-}, 30);
-
-timer()
-
-
-
+    })
+})
