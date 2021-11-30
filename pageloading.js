011d788d5d78b35f9e43cd42f864a56dde98bd3f
jazzc001 <jazzcheung212@gmail.com>
2021-11-30 00:03:58 +0000

    chainging

--- pageloading.js
+++ pageloading.js
@@ -1,8 +1,9 @@
+const percent = 0;
 function eatCount() {
   $(".monsterText").html("Click a<br>Monster to<br> Join a Room<br>to Chat");
 }
 
-const percent = 0;
+
 const timer = setInterval(function () {
     
   $(".bar").css("width", percent + "%");
